af872c518c170c3e3f1d6535ee1539d6875e2d70
ravellys <ravellyspyrrho@gmail.com>
2021-03-05 23:09:34 -0300

    templates name

---
+++ cap-2/templatesLiterais.js
@@ -0,0 +1,19 @@
+const book = {
+    name: 'Learning JS datastructure and algorithms'
+};
+
+console.log('You are reading' + book.name + '.,\n and this a new line\n and so this');
+
+//You are readingLearning JS datastructure and algorithms.,
+// and this a new line
+// and so this
+
+console.log(`You are reading ${book.name}
+and this a new line
+and so this
+`);
+
+// You are reading Learning JS datastructure and algorithms
+// and this a new line
+// and so this
+
