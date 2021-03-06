95d46772aadebf8f38e2c75586a2e3b08e54fd5b
ravellys <ravellyspyrrho@gmail.com>
2021-03-06 15:04:22 -0300

    modulos

--- cap-2/modulos/index.js
+++ cap-2/modulos/index.js
@@ -1,3 +1,9 @@
 import {circleArea, squareArea} from './calcArea.class.js';
+import Book from './book.js';
+
 console.log(circleArea(2));
 console.log(squareArea(2));
+console.log('\n');
+
+const myBook = new Book('title')
+myBook.printTitle();
