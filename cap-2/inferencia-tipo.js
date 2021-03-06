48e87a50a0525cc8d923f69a30e8d6b77bcd4f11
ravellys <ravellyspyrrho@gmail.com>
2021-03-06 16:04:48 -0300

    inferencia de tipos TS

---
+++ cap-2/inferencia-tipo.js
@@ -0,0 +1,9 @@
+// let age: number = 20;
+// let existFlag: boolean = true;
+// let language: string = 'JS';
+var age = 20;
+var existFlag = true;
+var language = 'JS';
+var favoriteLanguage; // usar só quando não for atribuido valores
+var langs = ['JS', 'Ruby', 'Python'];
+favoriteLanguage = langs[0];
---
+++ cap-2/inferencia-tipo.ts
@@ -0,0 +1,11 @@
+// let age: number = 20;
+// let existFlag: boolean = true;
+// let language: string = 'JS';
+
+let age = 20;
+let existFlag = true;
+let language = 'JS';
+
+let favoriteLanguage: string; // usar só quando não for atribuido valores
+let langs = ['JS', 'Ruby', 'Python'];
+favoriteLanguage = langs[0];
