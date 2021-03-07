a57250e0f81478cb0547edb755510aba3fbd1e35
ravellys <ravellyspyrrho@gmail.com>
2021-03-06 23:15:45 -0300

    inicializando array

---
+++ cap-3/inicializandoArrays.js
@@ -0,0 +1,12 @@
+// CRIANDO E INICIALIZANDO ARRAYS
+
+let daysOfWeek = new Array(); // declarar array com palavra reservada
+daysOfWeek = new Array(7); // especificando tamanho da linha
+daysOfWeek = new Array('dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab');
+
+// ou
+
+daysOfWeek = []
+daysOfWeek = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
+
+console.log(daysOfWeek.length) // 7
